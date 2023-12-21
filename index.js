5e8e78fdcf2e53d178dd62c6a94f42fbfc873b5e
Amalia <amaliaponcetoledo@gmail.com>
2023-12-21 10:07:00 +0100

    cambio de nombre a index

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/api/:date?', (req, res) => {
+    const { date } = req.params;
+
+    let parsedDate;
+    if (date) {
+        // Si se proporciona una cadena de fecha, intenta convertirla a Date
+        parsedDate = new Date(date);
+        // Si la fecha es un timestamp válido, convierte a Date
+        if (isNaN(parsedDate.getTime()) && !isNaN(Number(date))) {
+            parsedDate = new Date(Number(date));
+        }
+    } else {
+        // Si no se proporciona fecha, obtén la fecha actual
+        parsedDate = new Date();
+    }
+
+    if (isNaN(parsedDate.getTime())) {
+        res.json({ error: 'Invalid Date' });
+    } else {
+        // Devuelve el timestamp en formato Unix y UTC
+        res.json({
+            unix: parsedDate.getTime(),
+            utc: parsedDate.toUTCString()
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
